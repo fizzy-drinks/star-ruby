d57725c545de8449b1627ba6327d23cd74b9e556
chikorito <me@chikorito.land>
2024-12-14 11:54:08 -0300

    explicitly name properties in context validator

--- lib/routing/context_validator.rb
+++ lib/routing/context_validator.rb
@@ -12,12 +12,16 @@ module Star
 
       attr_reader :data, :properties
 
-      def method_missing(method, &block)
-        property = Builders::PropertyBuilder.build(method.to_s, &block)
-        value = map_to_type(property, data[method.to_s])
+      def method_missing(method, *, &block)
+        property(method, &block)
+      end
+
+      def property(name, &block)
+        property = Builders::PropertyBuilder.build(name.to_s, &block)
+        value = map_to_type(property, data[name.to_s])
 
         @properties ||= {}
-        @properties[method.to_s] = value
+        @properties[name.to_s] = value
       end
 
       def respond_to_missing? = true
